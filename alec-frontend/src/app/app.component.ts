c3e7a79d3f1e4cf59d1da593bb9cc6c3efaf34bd
qauseenmz <qmaab07@gmail.com>
2019-10-16 09:52:55 -0400

    Range Slider

--- alec-frontend/src/app/app.component.ts
+++ alec-frontend/src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
-import {NgbTimeStruct, NgbCalendar, NgbTimepickerConfig, NgbDateStruct, NgbPaginationModule, NgbAlertModule, NgbDateAdapter, NgbDateNativeAdapter} from '@ng-bootstrap/ng-bootstrap';
+import {NgbPaginationModule, NgbAlertModule} from '@ng-bootstrap/ng-bootstrap';
+import {NgbDateAdapter, NgbDateNativeAdapter} from '@ng-bootstrap/ng-bootstrap';
+
+import {NgbTimepickerConfig, NgbDateStruct} from '@ng-bootstrap/ng-bootstrap';
+import {NgbTimeStruct, NgbCalendar} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
@@ -10,8 +14,24 @@ export class AppComponent {
   title = 'alec-frontend';
   isSideBarMinimized = true;
   isSideBarVisible = true;
-  
+
+  minimizeSideBar(sideBarStatus){
+    this.isSideBarMinimized = sideBarStatus;
+  }
+
+  displaySideBar(sideBarStatus){
+    this.isSideBarVisible = sideBarStatus;
+    // alert(this.isSideBarVisible)
+  }
+
   time: NgbTimeStruct = {hour: 13, minute: 30, second: 0};
+
+  constructor(config: NgbTimepickerConfig, private calendar: NgbCalendar) { 
+     // customize default values of ratings used by this component tree
+     config.seconds = false;
+     config.spinners = false;
+  }
+
   model1: Date;
   model2: any = {
     "year": 2019,
@@ -19,19 +39,6 @@ export class AppComponent {
     "day": 23
   }
   model: NgbDateStruct;
-  
-  constructor(config: NgbTimepickerConfig, private calendar: NgbCalendar) { 
-     config.seconds = false;
-     config.spinners = false;
-  }
-  
-  minimizeSideBar(sideBarStatus){
-    this.isSideBarMinimized = sideBarStatus;
-  }
-
-  displaySideBar(sideBarStatus){
-    this.isSideBarVisible = sideBarStatus;
-  }
 
   get today() {
     return new Date();
--- alec-frontend/src/app/app.module.ts
+++ alec-frontend/src/app/app.module.ts
@@ -2,12 +2,14 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ChartsModule } from 'ng2-charts';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { MatSliderModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { SideNavbarComponent } from './side-navbar/side-navbar.component';
 import { HeaderComponent } from './header/header.component';
 import { ContentViewComponent } from './content-view/content-view.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -20,7 +22,9 @@ import { ContentViewComponent } from './content-view/content-view.component';
     NgbModule,
     FormsModule,  
     BrowserModule,
-    ChartsModule
+    ChartsModule,
+    BrowserAnimationsModule,
+    MatSliderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
